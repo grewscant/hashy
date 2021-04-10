a0a4240273e1831685047b5c002476b69d946815
Danyl Fernandes <androidan1304@gmail.com>
2021-04-10 07:30:41 -0700

    Add copy hash to clipboard

--- app/src/main/java/com/realllydan/hashy/MainActivity.java
+++ app/src/main/java/com/realllydan/hashy/MainActivity.java
@@ -2,6 +2,9 @@ package com.realllydan.hashy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +13,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private final String[] items = {"MD5", "SHA-1", "SHA-256"};
     private final static int FIRST_LIST_ITEM = 0;
 
+    private View rootLayout;
     private TextInputLayout tilInputString;
     private AutoCompleteTextView hashingMethodDropdown;
-    private TextView tvHashTitle, tvCalcHash;
+    private TextView tvHashTitle, tvCalcHash, tvInfo;
     private Button btnGenHash;
     private String userSelectedHashingMethod = items[FIRST_LIST_ITEM];
 
@@ -32,15 +37,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupUi() {
+        rootLayout = findViewById(R.id.rootLayout);
         tilInputString = findViewById(R.id.til_input_string);
         tvCalcHash = findViewById(R.id.tv_calc_hash);
         btnGenHash = findViewById(R.id.btn_gen_hash);
         tvHashTitle = findViewById(R.id.tv_hash_title);
+        tvInfo = findViewById(R.id.tv_info);
 
         setupHashingMethodDropdown();
         handleGenButtonClick();
 
         hideHashTextViews();
+        setupGenHashTextView();
     }
 
     private void setupHashingMethodDropdown() {
@@ -99,12 +107,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showHashTextViews() {
+        tvInfo.setVisibility(View.VISIBLE);
         tvHashTitle.setVisibility(View.VISIBLE);
         tvCalcHash.setVisibility(View.VISIBLE);
     }
 
     private void hideHashTextViews() {
+        tvInfo.setVisibility(View.GONE);
         tvHashTitle.setVisibility(View.GONE);
         tvCalcHash.setVisibility(View.GONE);
+    }
+
+    private void setupGenHashTextView() {
+        tvCalcHash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                copyTextToClipboard(tvCalcHash.getText().toString());
+                displayCopySuccess();
+            }
+        });
+    }
+
+    private void copyTextToClipboard(String textToCopy) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Hashed Message", textToCopy);
+        clipboard.setPrimaryClip(clip);
+    }
+
+    private void displayCopySuccess() {
+        Snackbar.make(rootLayout, "Hash copied to the clipboard!", Snackbar.LENGTH_LONG).show();
     }
 }
